466cc996b94f0b0a1762a599d2c9efa376376b27
Fabio <Fabio@ada-school.com>
2024-11-27 20:22:17 -0300

    Se agrego local Storage a ambos contextos

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,13 +3,28 @@ import { createContext, useState } from 'react'
 export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-    const login = (username, role) => setUser({username, role})
-    const logout = () => setUser(null)
-    
-    return (
-        <AuthContext.Provider value={{ user, login, logout }}>
-            { children }
-        </AuthContext.Provider>
-    )
+  const [user, setUser] = useState(() => {
+    // Restaurar del Local Storage
+    const storedUser = window.localStorage.getItem('user')
+    return storedUser ? JSON.parse(storedUser) : null
+  })
+
+  const login = (username, role) => {
+    const userData = { username, role }
+    setUser(userData)
+    // Guardar en el Local Storage
+    window.localStorage.setItem('user', JSON.stringify(userData))
+  }
+
+  const logout = () => {
+    setUser(null)
+    // Vaciar el Local Storage
+    window.localStorage.removeItem('user')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,16 +5,37 @@ export const CartContext = createContext()
 
 export const CartProvider = ({ children }) => {
   const { user } = useContext(AuthContext)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(() => {
+    // Restauramos el carrito del local Storage
+    const storedCart = window.localStorage.getItem('cart')
+    return storedCart ? JSON.parse(storedCart) : []
+  })
 
   const addToCart = (product) => {
     if (!user) window.alert('⚠ Debe iniciar sesión para poder agregar productos al carrito 🛒')
-    else setCart([...cart, product])
+    else {
+      // Verificar si existe el producto
+      // Si existe entonces sumarle al atributo cantidad +1
+      // Si no existe el producto, lo agrego pero {...product, quantity: 1}
+      const updatedCart = [...cart, product]
+      setCart(updatedCart)
+      // Guardar en el Local Storage
+      window.localStorage.setItem('cart', JSON.stringify(updatedCart))
+    }
   }
   const removeFromCart = (productId) => {
-    setCart(cart.filter(prod => prod.id !== productId))
+    const updatedCart = cart.filter(prod => prod.id !== productId)
+    setCart(updatedCart)
+    // Guardar en el Local Storage
+    window.localStorage.setItem('cart', JSON.stringify(updatedCart))
   }
-  const clearCart = () => setCart([])
+
+  const clearCart = () => {
+    // Vaciar el Local Storage
+    setCart([])
+    window.localStorage.removeItem('cart')
+  }
+
   const totalPrice = cart.reduce((sum, prod) => sum + prod.price, 0)
 
   return (
